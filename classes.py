4f809a11eb949f0e5d85c6cd068b7ebecb59a7d7
kainenw <108428180+kainenw@users.noreply.github.com>
2024-01-10 00:03:27 -0600

    separated counterpoint from cantus firmus lines

--- classes.py
+++ classes.py
@@ -1,8 +1,5 @@
 # %% [markdown]
 # # Analyze cf notes and features
-# 
-
-# %% [markdown]
 # ## Pitch Class (a single note)
 
 # %%
@@ -34,11 +31,6 @@ def is_consonant(num1, num2):
     else:
         return False
 
-print(is_consonant(2, 11))
-print(interval_class(0, 7))
-print(interval_class(7, 0))
-print(is_consonant(7, 26))
-
 # %% [markdown]
 # #### Motion Types (from the transition from two simultaneous notes to two other simultaneous notes)
 # - Parallel: 2
@@ -60,15 +52,8 @@ def motion(prev1, next1, prev2, next2):
     elif motion_value == 0:
       return 0
 
-
 def is_direct_perfect(prev1, next1, prev2, next2):
     next_interval = interval_class(next1, next2)
     is_direct = motion(prev1, next1, prev2, next2) > 0
     is_perfect = consonance["perfect"].__contains__(next_interval)
     return is_direct and is_perfect
-
-
-print(is_direct_perfect(2, 0, 11, 12))
-
-
-
